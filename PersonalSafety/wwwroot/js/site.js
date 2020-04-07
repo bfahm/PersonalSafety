18a81e92a48755df05fd367fabd6608a5a4473f4
bola <bola_fah.97@hotmail.com>
2020-04-07 13:27:52 +0200

    Added a force-recheck for updates button to update cookies when user updates the solution.

--- PersonalSafety/wwwroot/js/site.js
+++ PersonalSafety/wwwroot/js/site.js
@@ -1,4 +1,5 @@
 ﻿import { checkForQuery } from "../lib/app_lib.js";
+import { eraseCookie } from "../lib/app_lib.js";
 
 "use strict";
 
@@ -6,4 +7,10 @@ $(document).ready(function () {
     if (checkForQuery("needsUpdate")) {
         $('#updateModal').modal('show');
     }
+
+    $("#btn_recheck").click(function () {
+        eraseCookie("update_service_last_checked");
+        eraseCookie("update_service_is_updated");
+        window.location = "/"; 
+    });
 });
