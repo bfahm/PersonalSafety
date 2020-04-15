d1b25c0744f9ce589d6f532bd5659affbd19cfd3
bola <bola_fah.97@hotmail.com>
2020-04-15 21:03:19 +0200

    Attempt to debug registration problem

--- PersonalSafety/Services/Email/EmailServiceHelper.cs
+++ PersonalSafety/Services/Email/EmailServiceHelper.cs
@@ -1,62 +1,62 @@
-﻿using Microsoft.Extensions.Options;
-using PersonalSafety.Services.Otp;
+﻿using PersonalSafety.Services.Otp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PersonalSafety.Services
 {
     public class EmailServiceHelper
     {
-        public string RecepientMail { get; set; }
+        public string RecipientMail { get; set; }
         public string Token { get; set; }
         public string BaseUrl { get; set; }
         public string ActivationLink { get; set; }
         public string OTP { get; set; }
 
-        public EmailServiceHelper(string recepientMail, string token, string otp, string baseUrl, string endpoint)
+        public EmailServiceHelper(string recipientMail, string token, string otp, string baseUrl, string endpoint)
         {
-            RecepientMail = recepientMail;
+            RecipientMail = recipientMail;
             Token = token;
             BaseUrl = baseUrl;
             OTP = otp;
-            ActivationLink = BaseUrl + "/" + endpoint + "?email=" + RecepientMail + "&token=" + Uri.EscapeDataString(Token);
+            ActivationLink = BaseUrl + "/" + endpoint + "?email=" + RecipientMail + "&token=" + Uri.EscapeDataString(Token);
         }
 
         public List<string> SendEmail()
         {
             try
             {
-                using (MailMessage mail = new MailMessage())
+                using MailMessage mail = new MailMessage
                 {
-                    mail.From = new MailAddress("personalsafety20@gmail.com");
-                    mail.To.Add(RecepientMail);
-                    mail.Subject = "Confirmation for Personal Safety";
+                    From = new MailAddress("personalsafety20@gmail.com"),
+                    Subject = "Confirmation for Personal Safety",
+                    Body = GenerateMailBody(),
+                    IsBodyHtml = true
+                };
+                
+                mail.To.Add(RecipientMail);
 
-                    mail.Body = GenerateMailBody();
-                    mail.IsBodyHtml = true;
-
-                    using (SmtpClient smtp = new SmtpClient())
-                    {
-                        smtp.EnableSsl = true;
-                        smtp.UseDefaultCredentials = false;
-                        smtp.Credentials = new NetworkCredential("personalsafety20@gmail.com", "Test@123");
-                        smtp.Host = "smtp.gmail.com";
-                        smtp.Port = 587;
-                        smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        smtp.Send(mail);
-                    }
-                    return new List<string>();
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("personalsafety20@gmail.com", "Test@123");
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Send(mail);
                 }
+                return new List<string>();
             }catch(Exception ex)
             {
-                List<string> result = new List<string>();
-                result.Add(ex.Message);
-                result.Add(ex.InnerException.ToString());
+                List<string> result = new List<string>
+                {
+                    ex.Message, 
+                    ex.InnerException?.ToString(),
+                    ex.StackTrace
+                };
                 return result;
             }
             
@@ -65,7 +65,7 @@ namespace PersonalSafety.Services
         private string GenerateMailBody()
         {
             var body = new StringBuilder();
-            body.AppendFormat("Hello, {0}\n", RecepientMail);
+            body.AppendFormat("Hello, {0}\n", RecipientMail);
             body.AppendLine(@"It appears that you requested a confirmation request for your PersonalSafety Account, ");
             body.AppendLine("<a href=\" " + ActivationLink + " \">tap here to continue.</a>");
 
