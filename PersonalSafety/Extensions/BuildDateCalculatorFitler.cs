c4935bcfa4d0f48e0d9ce88d1f846777c6a95c0b
bola <bola_fah.97@hotmail.com>
2020-04-24 16:47:21 +0200

    New Feature: Auto scroll to NewContents first time after publishing.

---
+++ PersonalSafety/Extensions/BuildDateCalculatorFitler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PersonalSafety.Extensions
+{
+    public class BuildDateCalculatorFitler : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            const int peHeaderOffset = 60;
+            const int linkerTimestampOffset = 8;
+            byte[] bytes = new byte[2048];
+            using (FileStream file = new FileStream(Assembly.GetEntryAssembly().Location, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                file.Read(bytes, 0, bytes.Length);
+            }
+            Int32 headerPos = BitConverter.ToInt32(bytes, peHeaderOffset);
+            Int32 secondsSince1970 = BitConverter.ToInt32(bytes, headerPos + linkerTimestampOffset);
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime dateTimeUTC = dt.AddSeconds(secondsSince1970);
+            DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(dateTimeUTC, TimeZoneInfo.Local);
+            var result = localTime.ToString("dd-MMM-yyyy hh:mm:sstt") + " " + TimeZoneInfo.Local.Id;
+
+            context.HttpContext.Response.Cookies.Append("LastBuildDateServerSide", result);
+            context.HttpContext.Response.Cookies.Append("LastBuildDateNotifyServerSide", true.ToString());
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context){ }
+    }
+}
---
+++ PersonalSafety/wwwroot/js/home.js
@@ -0,0 +1,23 @@
+﻿import { eraseCookie } from "../lib/app_lib.js";
+import { setCookie } from "../lib/app_lib.js";
+import { getCookie } from "../lib/app_lib.js";
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".new_content").removeClass("new_content_animation");
+        $(".new_content").addClass("new_content_static");
+    }, 5000);
+
+    
+    var lastBuildDateClientSide = getCookie("LastBuildDateClientSide");
+    var lastBuildDateServerSide = getCookie("LastBuildDateServerSide");
+    var lastBuildDateNotifyServerSide = getCookie("LastBuildDateNotifyServerSide");
+    var lastBuildDateNotifyClientSide = getCookie("LastBuildDateNotifyClientSide");
+
+    if (((lastBuildDateClientSide !== lastBuildDateServerSide) || (lastBuildDateNotifyServerSide !== lastBuildDateNotifyClientSide))
+        && (lastBuildDateNotifyServerSide === "True")) {
+        $("#btn-new-contents").click();
+        setCookie("LastBuildDateClientSide", getCookie("LastBuildDateServerSide"), null);
+        setCookie("LastBuildDateNotifyClientSide", getCookie("LastBuildDateNotifyServerSide"), null);
+    }
+});
